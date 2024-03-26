7cabf1f2fb8acd4d9c97187fa6369fee512af8b4
Vinicius_VM <96557487+Vinicius-VM@users.noreply.github.com>
2024-03-26 09:40:23 -0300

    Add files via upload

--- sesit10-vite-main/src/App.jsx
+++ sesit10-vite-main/src/App.jsx
@@ -1,11 +1,28 @@
 
 import './App.css'
+import Card from './components/Card'
+import img1 from "../imgs/paisagem1.jpg"
+import img2 from "../imgs/colombo.webp"
 
 function App() {
 
   return (
     <>
-      <h1>Ola mundo!</h1>
+      <Card 
+        title="Paisagem 01"
+        image={img1}
+        text1="descricao"
+        text2="Curtiba/PR"/>
+      <Card 
+        title="Paisagem 02"
+        image={img2}
+        text1="descricao"
+        text2="Curtiba/PR"/>
+      <Card 
+        title="Paisagem 03"
+        image={img1}
+        text1="descricao"
+        text2="Curtiba/PR"/>
     </>
   )
 }
