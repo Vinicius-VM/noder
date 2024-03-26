0dfcc8af10f06b4f21b9a373daf084bad3fe94d4
Vinicius_VM <96557487+Vinicius-VM@users.noreply.github.com>
2024-03-26 12:22:54 -0300

    Add files via upload

--- sesit10-vite-main/src/App.jsx
+++ sesit10-vite-main/src/App.jsx
@@ -1,29 +1,48 @@
 
-import './App.css'
+import { Tilt } from 'react-tilt'
+
+import style from './App.module.css'
 import Card from './components/Card'
 import img1 from "../imgs/paisagem1.jpg"
 import img2 from "../imgs/colombo.webp"
 
 function App() {
+  const defaultOptions = {
+    reverse:        false,  // reverse the tilt direction
+    max:            35,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          1000,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
 
   return (
-    <>
+    <div className={style.section01}>
+      <tilt options={defaultOptions} style={{height: 250, width: 250}}>
       <Card 
         title="Paisagem 01"
         image={img1}
         text1="descricao"
         text2="Curtiba/PR"/>
+      </tilt>
+      <tilt options={defaultOptions} style={{height: 250, width: 250}}>
       <Card 
         title="Paisagem 02"
         image={img2}
         text1="descricao"
         text2="Curtiba/PR"/>
+      </tilt>
+      <tilt options={defaultOptions} style={{height: 250, width: 250}}>
       <Card 
-        title="Paisagem 03"
+        title="Paisagem 01"
         image={img1}
         text1="descricao"
         text2="Curtiba/PR"/>
-    </>
+      </tilt>
+    </div>
   )
 }
 
--- sesit10-vite-main/src/components/Card.jsx
+++ sesit10-vite-main/src/components/Card.jsx
@@ -1,6 +1,8 @@
+import style from './Card.module.css'
+
 export default function Card(props){
     return(
-        <div>
+        <div className={style.card}>
             <h2>{props.title}</h2>
             <img src={props.image} alt={props.title} width={200} height="auto"/>
             <p>{props.text1}</p>
